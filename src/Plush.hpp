b661a8f00b9f8966ebcae4eeb8a48130f4a9ecc9
ABEELACK ASHWIN <ashwin.abeelack@heckel02.u-pem.fr>
2023-01-26 11:55:16 +0100

    C-Les peluches,ça coute des sous

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -2,4 +2,22 @@
 
 class Plush{
 
+    public:
+        Plush()
+            : _cost{10} {
+
+            }
+
+    int get_cost() const
+    {   
+        return _cost;
+    }
+
+    void set_cost(int value)
+        {
+            _cost = value;
+        }
+
+    private:
+        int _cost;
 };
